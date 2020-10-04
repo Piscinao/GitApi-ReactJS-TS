9c5bc3d6b2e3ef50a65ba6a754b7da3662882f4e
Piscinao <henhvj@gmail.com>
2020-10-04 14:14:43 -0300

    Aplicanddo mais componentes de style

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,56 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+import { Title, Form, Repositories } from './styles';
+
 const Dashboard: React.FC = () => {
-  return <Title>Dashboard</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+
+      <Title>Explore repositórios no Github</Title>
+      {/* Form e repositories são componentes do style.ts */}
+      <Form action="">
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste" >
+          <img src="https://avatars0.githubusercontent.com/u/28152962?s=460&v=4" alt="Piscinão"/>
+
+        <div>
+          <strong>bootcamp/Gostack</strong>
+          <p>First reat app</p>
+        </div>
+
+        <FiChevronRight size={20}/>
+        </a>
+        <a href="teste" >
+          <img src="https://avatars0.githubusercontent.com/u/28152962?s=460&v=4" alt="Piscinão"/>
+
+        <div>
+          <strong>bootcamp/Gostack</strong>
+          <p>First reat app</p>
+        </div>
+
+        <FiChevronRight size={20}/>
+        </a>
+        <a href="teste" >
+          <img src="https://avatars0.githubusercontent.com/u/28152962?s=460&v=4" alt="Piscinão"/>
+
+        <div>
+          <strong>bootcamp/Gostack</strong>
+          <p>First reat app</p>
+        </div>
+
+        <FiChevronRight size={20}/>
+        </a>
+      </Repositories>
+
+    </>
+  );
 };
 
 
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,8 +1,138 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
 // Template literals -- template string
-// css do styled vale tanto para o reacjs e para o native
+// css do styled vale tanto para o reactjs e para o native
 export const Title = styled.h1`
   font-size: 48px;
   color: #3A3A3A;
+
+  /* Largura máxima quebra o texto */
+  max-width: 450px;
+  /* Leve distanciamento do texto */
+  line-height: 56px;
+
+  /* Espaçamento da parte de cima */
+  margin-top: 80px;
+
+
   `;
+
+// Style do form
+export const Form = styled.form`
+  /* Distanciamento do texto */
+  margin-top: 40px;
+  /* Tamanho máximo do form */
+  max-width: 700px;
+
+  /* Um ao lado do outro */
+  display: flex;
+
+  /* Fica flexível e ocupa o máximo do espaço possível */
+  input {
+    flex: 1;
+    height: 70px;
+    padding: 0 24px;
+    border: 0;
+    border-radius: 5px 0 0 5px;
+    color: #3A3A3A;
+
+    /* Alteração da cor do placeholder do input */
+    &::placeholder {
+      color: #A8A8b3;
+    }
+  }
+
+  /* Button style */
+  button {
+  /* Tamanho do botão */
+    width: 210px;
+    background: #04D361;
+    border-radius: 0px 5px 5px 0px;
+    border: 0;
+    color: #FFF;
+    font-weight: bold;
+    transition: background-color 0.2s;
+
+  /* Sempre que for colocar js no css coloca template string */
+  /* Usa a biblioteca polished para manipular a densidade da cor sem precisar do código  */
+  &:hover {
+    background: ${shade(0.2, '#04D361')};
+    }
+}
+
+`;
+
+
+export const Repositories = styled.div`
+  margin-top: 80px;
+  max-width: 700px;
+
+  /* a + a {
+      margin-top: 16px;
+  } */
+
+  a {
+    background: #FFF;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    display: block;
+    text-decoration: none;
+
+    display: flex;
+    align-items: center;
+    /* delay para ativação do hover */
+    transition: transform 0.2s;
+
+    /* distanciamento dos elementos a depois do primeiro */
+    & + a {
+      margin-top: 16px;
+    }
+    /* joga um pouco para direita */
+    &:hover {
+      transform: translateX(10px);
+    }
+
+
+    img {
+      /* Largura */
+      width: 64px;
+      /* Altura */
+      height: 64px;
+      /* Imagem arredondada */
+      border-radius: 50%;
+    }
+
+    div {
+      margin-left: 16px;
+
+      strong {
+        font-size: 20px;
+        color: #3D3D4D;
+      }
+
+      p {
+        font-size: 18px;
+        color: #A8A8B3;
+        margin-top: 4px;
+
+      }
+    }
+
+    /* Manipulando o ícone  */
+    svg {
+      /* todo espaço disponível da esquerda */
+      margin-left: auto;
+      color: #cbcbd6;
+    }
+
+
+
+
+
+  }
+
+`;
+
+
